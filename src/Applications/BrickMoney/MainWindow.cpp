945f0f681942eccc7ed6f83e4e6a5320c76003d8
Thomas Lämmlein <thomaslaemmlein@gmail.com>
2019-04-04 22:13:13 +0200

    First menu added

--- src/Applications/BrickMoney/MainWindow.cpp
+++ src/Applications/BrickMoney/MainWindow.cpp
@@ -6,9 +6,20 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    setWindowTitle(tr("BrickMoney - Software für LEGO Investment"));
+
+    ui->statusBar->showMessage(tr("BrickMoney gestartet"));
+
+    updateActions();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::updateActions()
+{
+    connect(ui->action_exit, &QAction::triggered, this, &MainWindow::close);
 }
--- src/Applications/BrickMoney/MainWindow.h
+++ src/Applications/BrickMoney/MainWindow.h
@@ -17,6 +17,8 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    void updateActions();
 };
 
 #endif // MAINWINDOW_H
