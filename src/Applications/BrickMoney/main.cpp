a1d0cfef61470ea133a464c0ebf2b69b9ead7c41
Thomas Lämmlein <thomaslaemmlein@gmail.com>
2020-04-18 20:28:14 +0200

    Force to be a window application.

--- src/Applications/BrickMoney/main.cpp
+++ src/Applications/BrickMoney/main.cpp
@@ -20,7 +20,10 @@ SET_LOGGER("BrickMoney.Main")
 using namespace log4cplus;
 using namespace log4cplus::helpers;
 
-
+// Ref: https://stackoverflow.com/questions/11785157/replacing-winmain-with-main-function-in-win32-programs
+#ifdef _MSC_VER
+#    pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+#endif
 
 int main(int argc, char *argv[])
 {
@@ -28,11 +31,11 @@ int main(int argc, char *argv[])
     log4cplus::Initializer initializer;
 
     SharedObjectPtr<Appender> console_appender(new ConsoleAppender(false, true));
-	console_appender->setName(LOG4CPLUS_TEXT("BrickMoneyConsole"));
+    console_appender->setName(LOG4CPLUS_TEXT("BrickMoneyConsole"));
 
-	log4cplus::tstring pattern = LOG4CPLUS_TEXT("%D{%Y-%m-%d %H:%M:%S,%q} [%t] %-5p %c - %m%n");
-	console_appender->setLayout(std::unique_ptr<Layout>(new PatternLayout(pattern)));
-	Logger::getRoot().addAppender(console_appender);
+    log4cplus::tstring pattern = LOG4CPLUS_TEXT("%D{%Y-%m-%d %H:%M:%S,%q} [%t] %-5p %c - %m%n");
+    console_appender->setLayout(std::unique_ptr<Layout>(new PatternLayout(pattern)));
+    Logger::getRoot().addAppender(console_appender);
 
     Logger::getRoot().setLogLevel(DEBUG_LOG_LEVEL);
 
