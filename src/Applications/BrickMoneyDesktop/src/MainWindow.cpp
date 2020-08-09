507da4c9d3e22346969dc7337e1660a6fe9a244f
Thomas Lämmlein <thomaslaemmlein@gmail.com>
2020-08-09 18:42:57 +0200

    All three lego sets can be displayed in QTableView

--- src/Applications/BrickMoneyDesktop/src/MainWindow.cpp
+++ src/Applications/BrickMoneyDesktop/src/MainWindow.cpp
@@ -1,11 +1,17 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 
+#include "Packages/BrickMoneyProject/BrickMoneyProject.h"
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->inStockTableView->setModel(BrickMoneyProject::Inst()->getInStockSortModel());
+    ui->forSaleTableView->setModel(BrickMoneyProject::Inst()->getForSaleSortModel());
+    ui->soldTableView->setModel(BrickMoneyProject::Inst()->getSoldSortModel());
 }
 
 MainWindow::~MainWindow()
